b83bcec56395b0e3c4bcfa0730a3edf890f7db5f
haoshan98 <haoshanw@gmail.com>
2020-08-13 16:45:18 +0800

    image-mapper import images

--- src/utils/image-mapper.js
+++ src/utils/image-mapper.js
@@ -1,22 +1,89 @@
-import sad_businessman from "../images/feeds/sad-businessman-sitting-head-hands-bed-dark-bedroom_41418-7.jpg";
-import crazy_teen from "../images/feeds/crazy-teen-playing-with-her-hair_1149-881.jpg";
-import get_enough_sleep from "../images/feeds/getenoughsleep.jpg";
-import conversation_concept from "../images/feeds/conversation-concept-illustration_114360-1102.jpg";
-import woman_eating_sandwich from "../images/feeds/woman-eating-sandwich_132075-20.jpg";
-import finger_mobile_phone from "../images/feeds/finger-mobile-phone_1057-3277.jpg";
-import tired_sad_young from "../images/feeds/tired-sad-young-pretty-woman-sitting-table-working-laptop-co-working-office-wearing-glasses-stress-work-funny-emotion-student-class-room-frustration_285396-94.jpg";
-import frustrated_young_dark from "../images/feeds/frustrated-young-dark-skinned-woman-with-shaggy-black-hair-frowning-looking-with-sad-unhappy-expression-while-going-through-hard-times-her-life-human-emotions-feelings_273609-1250.jpg";
-import progressive_muscle from "../images/feeds/Progressive muscle relaxation.jpg";
-import how_I_start_exercise from "../images/feeds/how-I-start-exercise.jpg";
+/* Feeds' images */
+import f01 from "../images/feeds/sad-businessman-sitting-head-hands-bed-dark-bedroom_41418-7.jpg";
+import f02 from "../images/feeds/puzzled-young-man-holding-forehead-while-feeling-stress_1262-18026.jpg";
+import f03 from "../images/feeds/crazy-teen-playing-with-her-hair_1149-881.jpg";
+import f04 from "../images/feeds/concerned-unhappy-people-feeling-anxiety_74855-5781.jpg";
+import f05 from "../images/feeds/cropped-view-hands-typing-laptop_1262-3196.jpg";
 
-import gameimg01 from "../images/games/AntiStress_Anxiety_Relief_Game.png";
-import gameimg02 from "../images/games/bubblewrap_android.png";
-import gameimg03 from "../images/games/flow_free.png";
-import gameimg04 from "../images/games/Pigment_Adult_Coloring_Book.png";
+import f06 from "../images/feeds/getenoughsleep.jpg";
+import f07 from "../images/feeds/conversation-concept-illustration_114360-1102.jpg";
+import f08 from "../images/feeds/portrait-student-girl-sitting-desk-biting-her-fist_1163-2568.jpg";
+import f09 from "../images/feeds/woman-eating-sandwich_132075-20.jpg";
+import f10 from "../images/feeds/young-soldier-clutched-fabric-military-uniform-second-world-war_76080-9480.jpg";
+
+import f11 from "../images/feeds/finger-mobile-phone_1057-3277.jpg";
+import f12 from "../images/feeds/post-traumatic-stress-disorder_34200-366.jpg";
+import f13 from "../images/feeds/frustrated-young-dark-skinned-woman-with-shaggy-black-hair-frowning-looking-with-sad-unhappy-expression-while-going-through-hard-times-her-life-human-emotions-feelings_273609-1250.jpg";
+import f14 from "../images/feeds/two-men-gossiping-cafe_10045-237.jpg";
+import f15 from "../images/feeds/tired-sad-young-pretty-woman-sitting-table-working-laptop-co-working-office-wearing-glasses-stress-work-funny-emotion-student-class-room-frustration_285396-94.jpg";
+
+import f16 from "../images/feeds/Progressive muscle relaxation.jpg";
+import f17 from "../images/feeds/how-I-start-exercise.jpg";
+import f18 from "../images/feeds/cute-brunette-student-woman-black-jacket-studying-reading-textbook-manual-university-library-having-hard-time-understanding-material-rolling-her-eyes-looking-bored-confused_273609-1133.jpg";
+import f19 from "../images/feeds/post-traumatic-stress-disorder_34200-367.jpg";
+import f20 from "../images/feeds/sad-man-holding-head-with-hand_1150-6345.jpg";
+
+import f21 from "../images/feeds/hands-holding-red-heart_49149-185.jpg";
+import f22 from "../images/feeds/fearful-young-caucasian-female-with-ginger-hair-dressed-white-blouse-having-confused-guilty-look-biting-her-lower-lip-feeling-sorry-doing-something-wrong-making-terrible-mistake_273609-48.jpg";
+import f23 from "../images/feeds/close-up-sad-young-woman-holding-her-head_23-2147953216.jpg";
+import f24 from "../images/feeds/deep-breath-exercise.jpg";
+import f25 from "../images/feeds/feed01.PNG";
+
+import f26 from "../images/feeds/side-tired-sad-office-worker-glasses-with-closed-eyes_74855-3294.jpg";
+import f27 from "../images/feeds/asian-women-are-sitting-hugging-their-knees-bed_7191-888.jpg";
+import f28 from "../images/feeds/Coping with stress.jpg";
+import f29 from "../images/feeds/red-heart-with-stethoscope-mint-green-background_23-2148129629.jpg";
+import f30 from "../images/feeds/feed03.PNG";
+
+import f31 from "../images/feeds/distressed-tired-middle-aged-male-facepalm_176420-18609.jpg";
+import f32 from "../images/feeds/asian-women-with-feelings-helplessness-hopelessness-white-bed-bedroom-either-insomnia-depression-symptoms-warning-signs-concept_41350-605.jpg";
+import f33 from "../images/feeds/lonely-girl-suffering-from-depression_74855-5270.jpg";
+import f34 from "../images/feeds/colorful-pills-plastic-bottle_23-2147983117.jpg";
+import f35 from "../images/feeds/IMG_0236.jpg";
+
+import f36 from "../images/feeds/ptsd-post-traumatic-stress-disorder_7547-467.jpg";
+import f37 from "../images/feeds/sad-woman_137573-2204.jpg";
+import f38 from "../images/feeds/Mental imagery relaxation.jpg";
+import f39 from "../images/feeds/tired-man-working-laptop_23-2148594474.jpg";
+import f40 from "../images/feeds/depression-stress-frustration-concept_140689-529.jpg";
+
+import f41 from "../images/feeds/girl-with-glasses-is-experiencing-post-traumatic-depression_118478-3.jpg";
+import f42 from "../images/feeds/young-woman-is-depressed-white-bed_1150-19255.jpg";
+import f43 from "../images/feeds/3d-render-wrecking-ball-demolishing-wall_1048-5707.jpg";
+
+/* Games' images */
+import ai01 from "../images/games/candy crush.png";
+import _i02 from "../images/games/bubblewrap-ios.png";
+import ai03 from "../images/games/Word Cross Puzzle.png";
+import ai04 from "../images/games/bejeweled-ios.png";
+import ai05 from "../images/games/wordscrape.png";
+
+import ai06 from "../images/games/Harmony-Melody Mirror.png";
+import _i07 from "../images/games/230x0w.png";
+import ai08 from "../images/games/Pigment - Adult Coloring Book.png";
+import a_09 from "../images/games/unnamed (2).png";
+import ai10 from "../images/games/drop7.jpg";
+
+import ai11 from "../images/games/AntiStress Anxiety Relief Game.png";
+import ai12 from "../images/games/jigsaw.png";
+import ai13 from "../images/games/Can Knockdown.png";
+import _i14 from "../images/games/Breath of Light.png";
+import ai15 from "../images/games/antistress.png";
+
+import ai16 from "../images/games/super mario.png";
+import ai17 from "../images/games/flow free.png";
+import ai18 from "../images/games/tetris.png";
+import ai19 from "../images/games/Thisissand.png";
+import a_20 from "../images/games/unnamed (1).png";
+
+import ai21 from "../images/games/blue.png";
+import a_22 from "../images/games/bubblewrap-android.png";
+import ai23 from "../images/games/Basketball Paper Office.png";
+
 
 export const mapImage = (name) => {
     switch(name){
         default:
-            return get_enough_sleep;
+            return f16;
     }
 };
