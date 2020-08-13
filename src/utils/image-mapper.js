254a5c1ecf31f839da1ee2859b908b02e29b6c95
haoshan98 <haoshanw@gmail.com>
2020-08-14 00:41:30 +0800

    image mapper update

--- src/utils/image-mapper.js
+++ src/utils/image-mapper.js
@@ -1,238 +1,233 @@
 /* Feeds' images */
-import f01 from "../images/feeds/sad-businessman-sitting-head-hands-bed-dark-bedroom_41418-7.jpg";
-import f02 from "../images/feeds/puzzled-young-man-holding-forehead-while-feeling-stress_1262-18026.jpg";
-import f03 from "../images/feeds/crazy-teen-playing-with-her-hair_1149-881.jpg";
-import f04 from "../images/feeds/concerned-unhappy-people-feeling-anxiety_74855-5781.jpg";
-import f05 from "../images/feeds/cropped-view-hands-typing-laptop_1262-3196.jpg";
+import f01 from "../images/feeds/f01.jpg";
+import f02 from "../images/feeds/f02.jpg";
+import f03 from "../images/feeds/f03.jpg";
+import f04 from "../images/feeds/f04.jpg";
+import f05 from "../images/feeds/f05.jpg";
 
-import f06 from "../images/feeds/getenoughsleep.jpg";
-import f07 from "../images/feeds/conversation-concept-illustration_114360-1102.jpg";
-import f08 from "../images/feeds/portrait-student-girl-sitting-desk-biting-her-fist_1163-2568.jpg";
-import f09 from "../images/feeds/woman-eating-sandwich_132075-20.jpg";
-import f10 from "../images/feeds/young-soldier-clutched-fabric-military-uniform-second-world-war_76080-9480.jpg";
+import f06 from "../images/feeds/f06.jpg";
+import f07 from "../images/feeds/f07.jpg";
+import f08 from "../images/feeds/f08.jpg";
+import f09 from "../images/feeds/f09.jpg";
+import f10 from "../images/feeds/f10.jpg";
 
-import f11 from "../images/feeds/finger-mobile-phone_1057-3277.jpg";
-import f12 from "../images/feeds/post-traumatic-stress-disorder_34200-366.jpg";
-import f13
-    from "../images/feeds/frustrated-young-dark-skinned-woman-with-shaggy-black-hair-frowning-looking-with-sad-unhappy-expression-while-going-through-hard-times-her-life-human-emotions-feelings_273609-1250.jpg";
-import f14 from "../images/feeds/two-men-gossiping-cafe_10045-237.jpg";
-import f15
-    from "../images/feeds/tired-sad-young-pretty-woman-sitting-table-working-laptop-co-working-office-wearing-glasses-stress-work-funny-emotion-student-class-room-frustration_285396-94.jpg";
+import f11 from "../images/feeds/f11.jpg";
+import f12 from "../images/feeds/f12.jpg";
+import f13 from "../images/feeds/f13.jpg";
+import f14 from "../images/feeds/f14.jpg";
+import f15 from "../images/feeds/f15.jpg";
 
-import f16 from "../images/feeds/Progressive muscle relaxation.jpg";
-import f17 from "../images/feeds/how-I-start-exercise.jpg";
-import f18
-    from "../images/feeds/cute-brunette-student-woman-black-jacket-studying-reading-textbook-manual-university-library-having-hard-time-understanding-material-rolling-her-eyes-looking-bored-confused_273609-1133.jpg";
-import f19 from "../images/feeds/post-traumatic-stress-disorder_34200-367.jpg";
-import f20 from "../images/feeds/sad-man-holding-head-with-hand_1150-6345.jpg";
+import f16 from "../images/feeds/f16.jpg";
+import f17 from "../images/feeds/f17.jpg";
+import f18 from "../images/feeds/f18.jpg";
+import f19 from "../images/feeds/f19.jpg";
+import f20 from "../images/feeds/f20.jpg";
 
-import f21 from "../images/feeds/hands-holding-red-heart_49149-185.jpg";
-import f22
-    from "../images/feeds/fearful-young-caucasian-female-with-ginger-hair-dressed-white-blouse-having-confused-guilty-look-biting-her-lower-lip-feeling-sorry-doing-something-wrong-making-terrible-mistake_273609-48.jpg";
-import f23 from "../images/feeds/close-up-sad-young-woman-holding-her-head_23-2147953216.jpg";
-import f24 from "../images/feeds/deep-breath-exercise.jpg";
+import f21 from "../images/feeds/f21.jpg";
+import f22 from "../images/feeds/f22.jpg";
+import f23 from "../images/feeds/f23.jpg";
+import f24 from "../images/feeds/f24.jpg";
 import f25 from "../images/feeds/feed01.PNG";
 
-import f26 from "../images/feeds/side-tired-sad-office-worker-glasses-with-closed-eyes_74855-3294.jpg";
-import f27 from "../images/feeds/asian-women-are-sitting-hugging-their-knees-bed_7191-888.jpg";
-import f28 from "../images/feeds/Coping with stress.jpg";
-import f29 from "../images/feeds/red-heart-with-stethoscope-mint-green-background_23-2148129629.jpg";
+import f26 from "../images/feeds/f26.jpg";
+import f27 from "../images/feeds/f27.jpg";
+import f28 from "../images/feeds/f28.jpg";
+import f29 from "../images/feeds/f29.jpg";
 import f30 from "../images/feeds/feed03.PNG";
 
-import f31 from "../images/feeds/distressed-tired-middle-aged-male-facepalm_176420-18609.jpg";
-import f32
-    from "../images/feeds/asian-women-with-feelings-helplessness-hopelessness-white-bed-bedroom-either-insomnia-depression-symptoms-warning-signs-concept_41350-605.jpg";
-import f33 from "../images/feeds/lonely-girl-suffering-from-depression_74855-5270.jpg";
-import f34 from "../images/feeds/colorful-pills-plastic-bottle_23-2147983117.jpg";
-import f35 from "../images/feeds/IMG_0236.jpg";
+import f31 from "../images/feeds/f31.jpg";
+import f32 from "../images/feeds/f32.jpg";
+import f33 from "../images/feeds/f33.jpg";
+import f34 from "../images/feeds/f34.jpg";
+import f35 from "../images/feeds/f35.jpg";
 
-import f36 from "../images/feeds/ptsd-post-traumatic-stress-disorder_7547-467.jpg";
-import f37 from "../images/feeds/sad-woman_137573-2204.jpg";
-import f38 from "../images/feeds/Mental imagery relaxation.jpg";
-import f39 from "../images/feeds/tired-man-working-laptop_23-2148594474.jpg";
-import f40 from "../images/feeds/depression-stress-frustration-concept_140689-529.jpg";
+import f36 from "../images/feeds/f36.jpg";
+import f37 from "../images/feeds/f37.jpg";
+import f38 from "../images/feeds/f38.jpg";
+import f39 from "../images/feeds/f39.jpg";
+import f40 from "../images/feeds/f40.jpg";
 
-import f41 from "../images/feeds/girl-with-glasses-is-experiencing-post-traumatic-depression_118478-3.jpg";
-import f42 from "../images/feeds/young-woman-is-depressed-white-bed_1150-19255.jpg";
-import f43 from "../images/feeds/3d-render-wrecking-ball-demolishing-wall_1048-5707.jpg";
+import f41 from "../images/feeds/f41.jpg";
+import f42 from "../images/feeds/f42.jpg";
+import f43 from "../images/feeds/f43.jpg";
 
 /* Games' images */
-import ai01 from "../images/games/candy crush.png";
-import _i02 from "../images/games/bubblewrap-ios.png";
-import ai03 from "../images/games/Word Cross Puzzle.png";
-import ai04 from "../images/games/bejeweled-ios.png";
-import ai05 from "../images/games/wordscrape.png";
+import ai01 from "../images/games/g01.png";
+import _i02 from "../images/games/g02.png";
+import ai03 from "../images/games/g03.png";
+import ai04 from "../images/games/g04.png";
+import ai05 from "../images/games/g05.png";
 
-import ai06 from "../images/games/Harmony-Melody Mirror.png";
-import _i07 from "../images/games/230x0w.png";
-import ai08 from "../images/games/Pigment - Adult Coloring Book.png";
-import a_09 from "../images/games/unnamed (2).png";
-import ai10 from "../images/games/drop7.jpg";
+import ai06 from "../images/games/g06.png";
+import _i07 from "../images/games/g07.png";
+import ai08 from "../images/games/g08.png";
+import a_09 from "../images/games/g09.png";
+import ai10 from "../images/games/g10.png";
 
-import ai11 from "../images/games/AntiStress Anxiety Relief Game.png";
-import ai12 from "../images/games/jigsaw.png";
-import ai13 from "../images/games/Can Knockdown.png";
-import _i14 from "../images/games/Breath of Light.png";
-import ai15 from "../images/games/antistress.png";
+import ai11 from "../images/games/g11.png";
+import ai12 from "../images/games/g12.png";
+import ai13 from "../images/games/g13.png";
+import _i14 from "../images/games/g14.png";
+import ai15 from "../images/games/g15.png";
 
-import ai16 from "../images/games/super mario.png";
-import ai17 from "../images/games/flow free.png";
-import ai18 from "../images/games/tetris.png";
-import ai19 from "../images/games/Thisissand.png";
-import a_20 from "../images/games/unnamed (1).png";
+import ai16 from "../images/games/g16.png";
+import ai17 from "../images/games/g17.png";
+import ai18 from "../images/games/g18.png";
+import ai19 from "../images/games/g19.png";
+import a_20 from "../images/games/g20.png";
 
-import ai21 from "../images/games/blue.png";
-import a_22 from "../images/games/bubblewrap-android.png";
-import ai23 from "../images/games/Basketball Paper Office.png";
+import ai21 from "../images/games/g21.png";
+import a_22 from "../images/games/g22.png";
+import ai23 from "../images/games/g23.png";
 
 
 export const mapImage = (name) => {
     switch (name) {
-        case "sad-businessman-sitting-head-hands-bed-dark-bedroom_41418-7.jpg":
+        case "f01.jpg":
             return f01;
-        case "puzzled-young-man-holding-forehead-while-feeling-stress_1262-18026.jpg":
+        case "f02.jpg":
             return f02;
-        case "crazy-teen-playing-with-her-hair_1149-881.jpg":
+        case "f03.jpg":
             return f03;
-        case "concerned-unhappy-people-feeling-anxiety_74855-5781.jpg":
+        case "f04.jpg":
             return f04;
-        case "cropped-view-hands-typing-laptop_1262-3196.jpg":
+        case "f05.jpg":
             return f05;
 
-        case "getenoughsleep.jpg":
+        case "f06.jpg":
             return f06;
-        case "conversation-concept-illustration_114360-1102.jpg":
+        case "f07.jpg":
             return f07;
-        case "portrait-student-girl-sitting-desk-biting-her-fist_1163-2568.jpg":
+        case "f08.jpg":
             return f08;
-        case "woman-eating-sandwich_132075-20.jpg":
+        case "f09.jpg":
             return f09;
-        case "young-soldier-clutched-fabric-military-uniform-second-world-war_76080-9480.jpg":
+        case "f10.jpg":
             return f10;
 
-        case "finger-mobile-phone_1057-3277.jpg":
+        case "f11.jpg":
             return f11;
-        case "post-traumatic-stress-disorder_34200-366.jpg":
+        case "f12.jpg":
             return f12;
-        case "frustrated-young-dark-skinned-woman-with-shaggy-black-hair-frowning-looking-with-sad-unhappy-expression-while-going-through-hard-times-her-life-human-emotions-feelings_273609-1250.jpg":
+        case "f13.jpg":
             return f13;
-        case "two-men-gossiping-cafe_10045-237.jpg":
+        case "f14.jpg":
             return f14;
-        case "tired-sad-young-pretty-woman-sitting-table-working-laptop-co-working-office-wearing-glasses-stress-work-funny-emotion-student-class-room-frustration_285396-94.jpg":
+        case "f15.jpg":
             return f15;
 
-        case "Progressive muscle relaxation.jpg":
+        case "f16.jpg":
             return f16;
-        case "how-I-start-exercise.jpg":
+        case "f17.jpg":
             return f17;
-        case "cute-brunette-student-woman-black-jacket-studying-reading-textbook-manual-university-library-having-hard-time-understanding-material-rolling-her-eyes-looking-bored-confused_273609-1133.jpg":
+        case "f18.jpg":
             return f18;
-        case "post-traumatic-stress-disorder_34200-367.jpg":
+        case "f19.jpg":
             return f19;
-        case "sad-man-holding-head-with-hand_1150-6345.jpg":
+        case "f20":
             return f20;
 
-        case "hands-holding-red-heart_49149-185.jpg":
+        case "f21.jpg":
             return f21;
-        case "fearful-young-caucasian-female-with-ginger-hair-dressed-white-blouse-having-confused-guilty-look-biting-her-lower-lip-feeling-sorry-doing-something-wrong-making-terrible-mistake_273609-48.jpg":
+        case "f22.jpg":
             return f22;
-        case "close-up-sad-young-woman-holding-her-head_23-2147953216.jpg":
+        case "f23.jpg":
             return f23;
-        case "deep-breath-exercise.jpg":
+        case "f24.jpg":
             return f24;
         case "feed01.PNG":
             return f25;
 
-        case "side-tired-sad-office-worker-glasses-with-closed-eyes_74855-3294.jpg":
+        case "f26.jpg":
             return f26;
-        case "asian-women-are-sitting-hugging-their-knees-bed_7191-888.jpg":
+        case "f27.jpg":
             return f27;
-        case "Coping with stress.jpg":
+        case "f28.jpg":
             return f28;
-        case "red-heart-with-stethoscope-mint-green-background_23-2148129629.jpg":
+        case "f29.jpg":
             return f29;
         case "feed03.PNG":
             return f30;
 
-        case "distressed-tired-middle-aged-male-facepalm_176420-18609.jpg":
+        case "f31.jpg":
             return f31;
-        case "asian-women-with-feelings-helplessness-hopelessness-white-bed-bedroom-either-insomnia-depression-symptoms-warning-signs-concept_41350-605.jpg":
+        case "f32.jpg":
             return f32;
-        case "lonely-girl-suffering-from-depression_74855-5270.jpg":
+        case "f33.jpg":
             return f33;
-        case "colorful-pills-plastic-bottle_23-2147983117.jpg":
+        case "f34.jpg":
             return f34;
-        case "IMG_0236.jpg":
+        case "f35.jpg":
             return f35;
 
-        case "ptsd-post-traumatic-stress-disorder_7547-467.jpg":
+        case "f36.jpg":
             return f36;
-        case "sad-woman_137573-2204.jpg":
+        case "f37.jpg":
             return f37;
-        case "Mental imagery relaxation.jpg":
+        case "f38.jpg":
             return f38;
-        case "tired-man-working-laptop_23-2148594474.jpg":
+        case "f39.jpg":
             return f39;
-        case "depression-stress-frustration-concept_140689-529.jpg":
+        case "f40.jpg":
             return f40;
 
-        case "girl-with-glasses-is-experiencing-post-traumatic-depression_118478-3.jpg":
+        case "f41.jpg":
             return f41;
-        case "young-woman-is-depressed-white-bed_1150-19255.jpg":
+        case "f42.jpg":
             return f42;
-        case "3d-render-wrecking-ball-demolishing-wall_1048-5707.jpg":
+        case "f43.jpg":
             return f43;
 
 
-        case "candy crush.png":
+        case "g01.png":
             return ai01;
-        case "bubblewrap-ios.png":
+        case "g02.png":
             return _i02;
-        case "Word Cross Puzzle.png":
+        case "g03.png":
             return ai03;
-        case "bejeweled-ios.png":
+        case "g04.png":
             return ai04;
-        case "wordscrape.png":
+        case "g05.png":
             return ai05;
 
-        case "Harmony-Melody Mirror.png":
+        case "g06.png":
             return ai06;
-        case "230x0w.png":
+        case "g07.png":
             return _i07;
-        case "Pigment - Adult Coloring Book.png":
+        case "g08.png":
             return ai08;
-        case "unnamed (2).png":
+        case "g09.png":
             return a_09;
-        case "drop7.jpg":
+        case "g10.png":
             return ai10;
 
-        case "AntiStress Anxiety Relief Game.png":
+        case "g11.png":
             return ai11;
-        case "jigsaw.png":
+        case "g12.png":
             return ai12;
-        case "Can Knockdown.png":
+        case "g13.png":
             return ai13;
-        case "Breath of Light.png":
+        case "g14.png":
             return _i14;
-        case "antistress.png":
+        case "g15.png":
             return ai15;
 
-        case "super mario.png":
+        case "g16.png":
             return ai16;
-        case "flow free.png":
+        case "g17.png":
             return ai17;
-        case "tetris.png":
+        case "g18.png":
             return ai18;
-        case "Thisissand.png":
+        case "g19.png":
             return ai19;
-        case "unnamed (1).png":
+        case "g20.png":
             return a_20;
 
-        case "blue.png":
+        case "g21.png":
             return ai21;
-        case "bubblewrap-android.png":
+        case "g22.png":
             return a_22;
-        case "Basketball Paper Office.png":
+        case "g23.png":
             return ai23;
 
         default:
